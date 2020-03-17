bf5f9149f88b99fce1e7ad5aa134985daae81b3b
David <david.vergison@gmail.com>
2020-03-17 12:29:50 +0100

    typo

--- bot.mjs
+++ bot.mjs
@@ -9,7 +9,7 @@ client.on('ready', () => {
 });
 
 function loadEmoji(list){
-  emojis = {s_success:"", s_failure:"", s_triumph_:"", s_despair:"", s_advantage:"", s_threat:"",s_lightside:"",s_darkside:""}
+  emojis = {s_success:"", s_failure:"", s_triumph:"", s_despair:"", s_advantage:"", s_threat:"",s_lightside:"",s_darkside:""}
 
   list.forEach((code, id) => {
     console.log(code['name']);
